9d477a9e489ac1c053bc2eb7247484f26dd99359
HumayunRChy <humayun.chowdhury@metropolia.fi>
2022-11-25 23:06:46 +0200

    new changes

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ my_number=[500, 100, 3000, 20000]
 add_seven(my_number)
 
 def add_seven_map(i):
-    return element+7
+    return element=element+7
 output_list= map(add_seven_map,i)
 print(output_list)
